756830fc0dde6a087678fad8906e781e668dbe24
Ballbot <5252bb@daum.net>
2025-03-01 23:46:18 +0900

    rls fu

--- app/reading/actions.ts
+++ app/reading/actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import { CreateReadingDto } from "@/utils/dto/CreateReadingDto";
+import { createClient } from "@/utils/supabase/server";
 import { HttpType } from "@/utils/types/HttpType";
 import { validate } from "class-validator";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function createReading(prevState: any, formData: FormData) {
   const dto = new CreateReadingDto();
@@ -10,13 +13,55 @@ export async function createReading(prevState: any, formData: FormData) {
   dto.length = formData.get("length")?.toString() ?? "";
   dto.originUrl = formData.get("originUrl")?.toString() ?? "";
   dto.guestId = formData.get("guestId")?.toString() ?? "";
+  const forwardedFor = (await headers()).get("x-forwarded-for");
+  const ipAddress = forwardedFor ? forwardedFor.split(",")[0] : null;
 
   const errors = await validate(dto);
   if (errors.length > 0) {
     return {
       code: 400,
-      message: "ValidationFailed",
+      message: "입력 값이 잘못되었습니다.",
       detail: JSON.stringify(errors),
     } as HttpType;
   }
+
+  let supabase = await createClient({ admin: true });
+  const { data: sessionData } = await supabase.auth.getUser();
+
+  if (!sessionData.user && !ipAddress) {
+    return {
+      code: 400,
+      message: "IP 검증에 실패했습니다. 로그인 후 이용해주세요.",
+      detail: "ipAddress field is null",
+    };
+  }
+
+  supabase = await createClient({ admin: false });
+  const { data: readingData, error } = await supabase
+    .from("readings")
+    .insert([
+      {
+        original_url: dto.originUrl,
+        owner: sessionData.user ? sessionData.user.id : null,
+        guest_id: sessionData.user ? null : dto.guestId,
+        ip_address: ipAddress,
+        level: dto.level,
+        length: dto.length,
+        status: "PENDING",
+        error_reason: null,
+        content: null,
+      },
+    ])
+    .select();
+
+  console.log(error);
+  if (error || readingData.length !== 1) {
+    return {
+      code: 500,
+      message: "InternalServerError",
+      detail: JSON.stringify(error),
+    };
+  }
+
+  redirect(`/reading/${readingData[0].id}`);
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,15 +1,24 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { Database } from "../types/database.types";
 
-export async function createClient() {
+type CreateClientProps = {
+  admin?: boolean;
+};
+
+export async function createClient(props?: CreateClientProps) {
   const cookieStore = await cookies();
+  console.log(process.env.SUPABASE_SERVICE_ROLE_KEY);
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.SUPABASE_SERVICE_ROLE_KEY as string,
     {
       cookies: {
         getAll() {
+          if (props?.admin) {
+            return [];
+          }
           return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
